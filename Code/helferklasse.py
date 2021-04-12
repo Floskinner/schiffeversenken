ca6df28eebba59bc998030d6efe606a957531b83
Floskinner <58706771+Floskinner@users.noreply.github.com>
2021-04-12 15:08:08 +0200

    Add Enum for Status and Richtung

---
+++ Code/helferklasse.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class Status(Enum):
+    DANEBEN = -1
+    WASSER = 0
+    TREFFER = 1
+    SCHIFF = 2
+
+class Richtung(Enum):
+    NORDEN = 0
+    OSTEN = 1
+    SUEDEN = 2
+    WESTEN = 3
